bc2831bcecf774ca8e457ec16f39bea6ee934ae5
CarlesVila <carles.vd@gmail.com>
2019-01-23 22:09:24 +0100

    Working in data map

--- src/Components/diplomacy/Map/index.js
+++ src/Components/diplomacy/Map/index.js
@@ -3,10 +3,24 @@ import { SvgLoader, SvgProxy } from 'react-svgmt';
 import MapSVG from './../../../img/DiplomacyMap.svg';
 import army from './../../../img/army.svg';
 import fleet from './../../../img/fleet.svg'
+import datamap from './../../../data/map';
 
 class Map extends Component {
     componentDidMount (){
-
+        
+    }
+    loadArmy = () => {
+        var armys = []
+        for(const mapid in datamap){
+            const country = datamap[mapid];
+            if(country.kind==='land'){
+                armys.push(<SvgLoader key={'army'+country.id} ref="army" className="army" id={'army'+country.id} path={army} onClick={()=>this.ArmyDroped('army'+country.id)} style={{top:`${country.y}pt`,left:`${country.x}pt`}}/>)
+            }
+            else{
+                armys.push(<SvgLoader key={'fleet'+country.id} ref="fleet" className="fleet" id={'fleet'+country.id} path={fleet} onClick={()=>this.ArmyDroped('fleet'+country.id)} style={{top:`${country.y}pt`,left:`${country.x}pt`}}/>)
+            }
+        }
+        return armys;
     }
     TerritoryClicked = (event) =>{
         console.log(event.target.id);
@@ -22,8 +36,7 @@ class Map extends Component {
                     <SvgProxy selector=".w" fill="#99CCFF" stroke="black" />
                     <SvgProxy selector=".s" fill="#DDDDDD" stroke="black" />
                 </SvgLoader>
-                <SvgLoader ref="army" className="army" id="army" path={army} onClick={()=>this.ArmyDroped('army')} style={{top:'460pt',left:'500pt'}}/>
-                <SvgLoader ref="fleet" className="fleet" id="fleet" path={fleet} onClick={()=>this.ArmyDroped('fleet')} style={{top: '250pt',left:'308pt', fill: 'blue'}}/>
+                {this.loadArmy()}
             </div>
         );
     }
--- src/data/map.js
+++ src/data/map.js
@@ -3,7 +3,7 @@ export default {
         name:'Norwegian Sea',
         id: 'NWG',
         x:220,
-        y:60,
+        y:50,
         kind: 'sea',
         coast: false,
         neighbors: ['NAO','Cly','Edi','NTH','Nwy','BAR']
@@ -12,7 +12,7 @@ export default {
         name: 'North Sea',
         id: 'NTH',
         x: 190,
-        y: 60,
+        y: 220,
         kind: 'sea',
         coast: false,
         neighbors: ['NWG','Edi','Yor','Lon','Bel','Hol','HEL','Den','SKA','Nwy']
@@ -30,7 +30,7 @@ export default {
         name: 'Aegean Sea',
         id: 'AEG',
         x: 392,
-        y: 500,
+        y: 505,
         kind: 'sea',
         coast: false,
         neighbors: ['ION','Gre','Bul','Con','Smy','EAS']
@@ -47,11 +47,109 @@ export default {
     Ank:{
         name: 'Ankara',
         id: 'Ank',
-        x: 500,
-        y: 445,
+        x: 490,
+        y: 450,
         kind: 'land',
         coast: true,
         neighbors: ['BLA','Arm','Smy','Con']
     },
-
+    Apu:{
+        name: 'Apulia',
+        id: 'Apu',
+        x: 280,
+        y: 455,
+        kind: 'land',
+        coast: true,
+        neighbors: ['ION','ADR','Ven','Rom','Nap']
+    },
+    Arm:{
+        name: 'Armenia',
+        id: 'Arm',
+        x: 575,
+        y: 445,
+        kind: 'land',
+        coast: true,
+        neighbors: ['BLA','Ank','Smy','Syr','Sev']
+    },
+    BAL:{
+        name: 'Baltic Sea',
+        id: 'BAL',
+        x: 208,
+        y: 250,
+        kind: 'sea',
+        coast: false,
+        neighbors: ['BOT','Lvn','Pru','Ber','Kie','Den','Swe']
+    },
+    BAR:{
+        name: 'Barents Sea',
+        id: 'BAR',
+        x: 440,
+        y: 30,
+        kind: 'sea',
+        coast: false,
+        neighbors: ['NWG','Nwy','Stp']
+    },
+    Bel:{
+        name: 'Belgium',
+        id: 'Bel',
+        x: 190,
+        y: 305,
+        kind: 'land',
+        coast: true,
+        neighbors: ['NTH','ENG','Pic','Bur','Ruh','Hol']
+    },
+    Ber:{
+        name: 'Berlin',
+        id: 'Ber',
+        x: 272,
+        y: 270,
+        kind: 'land',
+        coast: true,
+        neighbors: ['BAL','Kie','Mun','Sil','Pru']
+    },
+    BLA:{
+        name: 'Black Sea',
+        id: 'BLA',
+        x: 470,
+        y: 425,
+        kind: 'sea',
+        coast: false,
+        neighbors: ['Con','Bul','Rum','Sev','Arm','Ank']
+    },
+    Boh:{
+        name: 'Bohemia',
+        id: 'Boh',
+        x: 283,
+        y: 325,
+        kind: 'land',
+        coast: false,
+        neighbors: ['Mun','Tyr','Vie','Gal','Sil']
+    },
+    Bre:{
+        name: 'Brest',
+        id: 'Bre',
+        x: 120,
+        y: 325,
+        kind: 'land',
+        coast: true,
+        neighbors: ['MAO','ENG','Pic','Par','Gas']
+    },
+    Bud:{
+        name: 'Budapest',
+        id: 'Bud',
+        x: 350,
+        y: 370,
+        kind: 'land',
+        coast: false,
+        neighbors: ['Tri','Vie','Gal','Rum','Ser']
+    },
+    Bul:{
+        name: 'Bulgaria',
+        id: 'Bul',
+        x: 395,
+        y: 450,
+        kind: 'land',
+        coast: true,
+        neighbors: ['NTH','ENG','Pic','Bur','Ruh','Hol']
+    },
 }
