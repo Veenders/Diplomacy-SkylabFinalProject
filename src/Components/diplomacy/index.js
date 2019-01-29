1cc1da1c7fa100198df009a3af0c46b873581365
CarlesVila <carles.vd@gmail.com>
2019-01-29 23:59:57 +0100

    Finish Movement and Change Turn

--- src/Components/diplomacy/index.js
+++ src/Components/diplomacy/index.js
@@ -46,21 +46,19 @@ class Diplomacy extends Component {
         const indexarmy = prturn.userturn[indexuserturn].armies.findIndex(army => army.id === order.id);
         prturn.userturn[indexuserturn].armies[indexarmy].territory = order.destination;
     }
-    solveCombat(orders, order, samedestination){
-        let result='tie'
-        const combat = [{...order, strength: 1}];
-        samedestination.forEach(element => {
-            const index = orders.findIndex(prorder => prorder.id===element.id)
-            orders.splice(index,1);
-            combat.push({...element, strength: 1})
-        })
-        const presupportorders = orders.filter(prorder=>prorder.destination===order.destination&&prorder.order==='support')
+    startCrossCombat(orders, order, crossorder){
+        let result='tie';
+        let retired = []
+        let retir=[]
+        const combat = [{...order, strength: 1},{...crossorder, strength: 1}];
+        const presupportorders = orders.filter(prorder=>(prorder.destination===order.destination||prorder.destination===crossorder.destination)&&prorder.order==='support')
         if(presupportorders>0){
             const supportorders = []
             presupportorders.forEach(support => {
                 let result = '';
                 const samedestin = orders.filter(prorder => prorder.destination===support.origin);
-                samedestin.length===0?supportorders.push(support):[orders,result]=this.solveCombat(orders,support,samedestin)
+                samedestin.length===0?supportorders.push(support):[orders,result,retir]=this.startCombat(orders,support,samedestin)
+                retired.push(retir);
                 result ==='win' && supportorders.push(support)
             })
             supportorders.forEach(support=>{
@@ -69,51 +67,145 @@ class Diplomacy extends Component {
             })
         }
         combat.sort(function (combat1,combat2) {
-            if (combat1.strength > combat2.strength) { //comparación lexicogŕafica
+            if (combat1.strength > combat2.strength) {
               return 1;
             } else if (combat1.strength < combat2.strength) {
               return -1;
             } 
             return 0;
-          });
-        console.log(combat)
-        return [orders,result];
+        });
+        if(combat[0].strenght>combat[1].strenght){
+            combat[0].id === order.id && (result='win');
+            combat[0].order === 'move' && this.solveOrder(combat[0]);
+            for(let i = 1; i<combat.length; i++ ){
+                combat[i].origin === combat[0].destination && combat[0].order === 'move' && retired.push({...combat[i], attack:combat[0].origin})
+            }
+        }
+        return [orders,result,retired];
+    }
+    startCombat(orders, order, samedestination, key='destination'){
+        let result='tie'
+        let retired = []
+        let retir=[]
+        const combat = [{...order, strength: 1}];
+        samedestination.forEach(element => {
+            const index = orders.findIndex(prorder => prorder.id===element.id)
+            orders.splice(index,1);
+            combat.push({...element, strength: 1})
+        })
+        const presupportorders = orders.filter(prorder=>prorder.destination===order[key]&&prorder.order==='support')
+        if(presupportorders>0){
+            const supportorders = []
+            presupportorders.forEach(support => {
+                let result = '';
+                const samedestin = orders.filter(prorder => prorder.destination===support.origin && prorder.order==='move');
+                samedestin.length===0?supportorders.push(support):[orders,result,retired]=this.startCombat(orders,support,samedestin,'origin')
+                retired.push(retir);
+                result ==='win' && supportorders.push(support)
+            })
+            supportorders.forEach(support=>{
+                const index = combat.findIndex(combatarmy=>combatarmy.origin===support.support);
+                combat[index].strength++;
+            })
+        }
+        combat.sort(function (combat1,combat2) {
+            if (combat1.strength > combat2.strength) {
+              return 1;
+            } else if (combat1.strength < combat2.strength) {
+              return -1;
+            } 
+            return 0;
+        });
+        if(combat[0].strenght>combat[1].strenght){
+            combat[0].id === order.id && (result='win');
+            combat[0].order === 'move' && this.solveOrder(combat[0]);
+            for(let i = 1; i<combat.length; i++ ){
+                combat[i].origin === combat[0].destination && combat[0].order === 'move' && retired.push({...combat[i], attack:combat[0].origin})
+            }
+        }
+        return [orders,result,retired];
     }
     solveMovement=(orders,order)=>{
-        let solved = true;
+        let solved = 'tie';
+        let retired = []
+        let retir=[]
         const emptydestination = orders.findIndex(prOrder => prOrder.origin === order.destination)
         if(emptydestination === -1){
             const samedestination = orders.filter(prorder => prorder.destination===order.destination);
-            samedestination.length===0?this.solveOrder(order):[orders] = this.solveCombat(orders, order, samedestination);
+            samedestination.length===0?this.solveOrder(order)&&(solved='win'):[orders,solved,retir] = this.startCombat(orders, order, samedestination);
+            retired.push(...retir);
         }else{
             const destinationOrder = orders[emptydestination]
             if(destinationOrder.order==='move'){
                 if(destinationOrder.destination===order.origin){
-                    [orders] = this.solveCombat(orders, order, [destinationOrder]);
+                    [orders,solved,retir] = this.startCrossCombat(orders, order, destinationOrder);
+                    retired.push(...retir);
                 }else{
                     let [destinOrder] = orders.splice(emptydestination,1);
-                    [orders, solved]=this.solveMovement(orders,destinationOrder)
-                    solved && this.solveOrder(order);
-                    !solved && orders.push(destinOrder) && ([orders] = this.solveCombat(orders, order, [destinationOrder]));
+                    [orders, solved, retir]=this.solveMovement(orders,destinationOrder)
+                    retired.push(...retir);
+                    solved==='win' && this.solveOrder(order);
+                    solved!=='win' && orders.push(destinOrder) && ([orders,solved,retir] = this.startCombat(orders, order, [destinationOrder]));
+                    retired.push(...retir);
                 }
             }else{
-                [orders] = this.solveCombat(orders, order, [destinationOrder]);
+                [orders,solved,retir] = this.startCombat(orders, order, [destinationOrder]);
+                retired.push(...retir);
             }
         }
-        return [orders,solved];
+        return [orders,solved,retired];
     }
-    processTurn = () =>{
+    processTurn = async() =>{
         const {game, turn} = this.state
         const prturn = game.turns[turn]
         let orders = []
+        let retired = []
         prturn.userturn.map(userturn=>userturn.orders.map(order => orders.push({country: userturn.country, ...order})))
         let moveIndex = orders.findIndex(order=> order.order === 'move');
         while(moveIndex !== -1){
             const [order]= orders.splice(moveIndex,1);
-            [orders] = this.solveMovement(orders,order);
+            let retir='';
+            [orders,,retir] = this.solveMovement(orders,order);
+            retired.push(...retir);
             moveIndex = orders.findIndex(order=> order.order === 'move');
         }
-        
+        if(retired>0){
+            prturn.phase=2;
+            const result = await DBService.setDocumentWithId('diplomacy', game, game.id);
+            result && this.setState({game,turn,loading:false});
+        }else{
+            if(prturn.seasson==='autumn'){
+                prturn.phase=3;
+                const result = await DBService.setDocumentWithId('diplomacy', game, game.id);
+                result && this.setState({game,turn,loading:false});
+            }else{
+                prturn.phase=4;
+                const result = await DBService.setDocumentWithId('diplomacy', game, game.id);
+                result && this.setState({game,turn,loading:false},this.newTurn);
+            }
+        }
+    }
+    newTurn = async() =>{
+        this.setState({loading: true});
+        const {game, turn} = this.state
+        const prturn = game.turns[turn]
+        const newturn ={}
+        newturn.id = prturn.id;
+        newturn.phase = 1;
+        if(prturn.season==='autumn'){
+            newturn.season = 'spring';
+            newturn.year=prturn+1;
+        }else{
+            newturn.season = 'autumn'
+        }
+        newturn.userturn =[]; 
+        prturn.userturn.forEach(pruserturn =>{
+            const {armies, country, player, territories} = pruserturn
+            newturn.userturn.push({armies, country, player, territories, finishedOrders: false, orders:[]})
+        })
+        game.turns.push(newturn);
+        const result = await DBService.setDocumentWithId('diplomacy', game, game.id);
+        result && this.setState({game,turn:game.turns.length-1,loading:false});
     }
     render() {
         const {user} = this.props
@@ -124,13 +216,17 @@ class Diplomacy extends Component {
         if(loading){
             return <Loading />
         }
+        const prturn = game.turns[turn];
         return (
             <div className="GameBoard">
                 <h1>{game.name}</h1>
                 <div className="Board">
-                    <Map turn={game.turns[turn]} player={user.id} saveOrders={this.saveOrders}/>
+                    <Map turn={prturn} player={user.id} saveOrders={this.saveOrders}/>
                     <div className="Complements">
-                        <Orders turn={game.turns[turn]} player={user.id} saveOrders={this.saveOrders} processTurn={this.processTurn}/>
+                        {prturn.phase===1 && <Orders turn={prturn} player={user.id} saveOrders={this.saveOrders} processTurn={this.processTurn}/>}
+                        {prturn.phase===2 && 'retreats'}
+                        {prturn.phase===3 && 'Create Units'}
+                        {prturn.phase===4 && 'Finished Turn'}
                         <Messenger players={game.players} idgame={game.id} from={user.id}/>
                     </div>
                 </div>
@@ -146,48 +242,3 @@ const mapStateToProps = (state) => {
 }
 
 export default withRouter(connect(mapStateToProps)(Diplomacy));
-
-/*
-let preSupportOrders = orders.filter(order=> order.order === 'support');
-        let supportOrders = []
-        let combatorders = {}
-        while(preSupportOrders>0){
-            const order = preSupportOrders.shift()
-            const index = orders.findIndex(prorder => prorder.id === order.id);
-            orders.splice(index,1);
-            if(orders.findIndex(prOrder => prOrder.destination === order.origin && prOrder.order ==='move')){
-                combatorders[order.origin] = [order];
-                const samedestination = orders.filter(prorder => prorder.destination===order.origin)
-                for(let i = 0; i<samedestination.length;i++){
-                    const index = orders.findIndex(prorder => prorder.destination === order.origin);
-                    const combatorder = orders.splice(index,1);
-                    combatorders[order.origin].push(combatorder);
-                }
-            }else{
-                supportOrders.push(order);
-            }
-        }
-        let preresolvedorders = []
-        while(orders.length > 0){
-            const order = orders.shift()
-            if(orders.findIndex(prorder=>prorder.destination === order.destination || (prorder.origin === order.destination && prorder.destination===order.origin))===-1){
-                preresolvedorders.push(order);
-            }else{
-                combatorders[order.destination] = [order];
-                const samedestination = orders.filter(prorder => prorder.destination===order.destination || (prorder.origin === order.destination && prorder.destination===order.origin));
-                for(let i = 0; i<samedestination.length;i++){
-                    const index = orders.findIndex(prorder => prorder.destination === order.destination || (prorder.origin === order.destination && prorder.destination===order.origin));
-                    const combatorder = orders.splice(index,1);
-                    combatorders[order.destination].push(combatorder);
-                }
-            }
-        }
-        for(const territory in combatorders){
-            const suporting = supportOrders.filter(order => order.support === territory);
-            if(suporting.length>0){
-
-            }
-        }
-        console.log('Processed Turn',prturn)
-        console.log('Final elements',preresolvedorders,combatorders);
-*/
